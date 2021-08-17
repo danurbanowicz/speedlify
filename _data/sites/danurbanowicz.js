af87c459be8f124d0644c0cf5fe0b8cd73d8f643
Dan Urbanowicz <dan.urbanowicz@gmail.com>
2021-08-17 10:20:23 +0200

    Add first test target

--- _data/sites/danurbanowicz.js
+++ _data/sites/danurbanowicz.js
@@ -1,26 +1,18 @@
 module.exports = {
-	description: "Front-end Testing Tools",
+	name: "danurbanowicz.com", // optional, falls back to object key
+	description: "Dan's web site",
 	// skip if localhost
 	// skip if this is a new fork of the speedlify (not Zach’s)
 	skip: !process.env.CONTEXT || process.env.SITE_NAME !== "speedlify",
 	options: {
-		frequency: 60 * 11 + 30, // 11h, 30m
+		frequency: 60 * 23, // 23 hours
 		// Use "run" if the sites don’t share assets on the same origin
 		//           and we can reset chrome with each run instead of
 		//           each site in every run (it’s faster)
 		// Use "site" if sites are all on the same origin and share assets.
-		freshChrome: "site",
+		freshChrome: "site"
 	},
 	urls: [
-		"https://eslint.org/",
-		"https://qunitjs.com/",
-		"https://karma-runner.github.io/latest/index.html",
-		"https://gulpjs.com/",
-		"https://webhint.io/",
-		"https://gruntjs.com/",
-		"https://theintern.io/",
-		"https://istanbul.js.org/",
-		"https://webdriver.io/",
-		"https://mochajs.org/"
+		"https://www.danurbanowicz.com/"
 	]
 };
